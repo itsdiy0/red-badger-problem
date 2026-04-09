56d353529e9b8a580764f090dc2d851ad0bb9022
itsdiy0 <diyo11jen@gmail.com>
2026-04-09 18:03:58 +0100

    init: project structure, input parsing and base classes + added .gitignore

---
+++ main.py
@@ -0,0 +1,36 @@
+ORIENTATIONS = ['N', 'E', 'S', 'W']  
+
+class Grid:
+    def __init__(self, max_x, max_y):
+        self.max_x = max_x
+        self.max_y = max_y
+        self.scents = set()
+
+class Robot:
+    def __init__(self, x, y, orientation):
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+        self.lost = False
+
+def parse_input(data):
+    lines = [line.strip() for line in data.strip().splitlines() if line.strip()]
+    max_x, max_y = map(int, lines[0].split())
+    grid = Grid(max_x, max_y)
+    robots = []
+    i = 1
+    while i < len(lines):
+        x, y, orientation = lines[i].split()
+        instructions = lines[i + 1]
+        robots.append((Robot(int(x), int(y), orientation), instructions))
+        i += 2
+    return grid, robots
+
+
+if __name__ == "__main__":
+    import sys
+    data = sys.stdin.read()
+    grid, robots = parse_input(data)
+    print(grid.max_x, grid.max_y)
+    for robot, instructions in robots:
+        print(robot.x, robot.y, robot.orientation, instructions)
